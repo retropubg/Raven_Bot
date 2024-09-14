9ca701ff4fb9bca3842425f172f847e370c44657
DanDaSG <dandasg2901@gmail.com>
2024-09-14 13:48:54 -0500

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'D
 if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'ElRDAN');
 if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6409081562');
 
-if (!defined('BOT_NAME'))               define('BOT_NAME', 'BETA');
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'SrDan');
 if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'ElSrDan_bot');
 if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7069082200:AAFNkKFir6Q0FKSS2dhj884-2u9Fj01hlJs');
 if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-1002232703186');
