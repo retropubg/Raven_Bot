ba479dd74ae75e0721eb6227571276f79fb0dd5e
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-13 13:58:29 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
-if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'Dan');
-if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'ElRDAN');
-if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6409081562');
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'retro);
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'eretro_7);
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6699273462');
 
-if (!defined('BOT_NAME'))               define('BOT_NAME', 'SrDan');
-if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'ElSrDan_bot');
-if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7069082200:AAFNkKFir6Q0FKSS2dhj884-2u9Fj01hlJs');
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'retrobot');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'retrochkbot);
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7633669044:AAHiS9PWSZkyZeHdTA6m8-6PYNf9DKitfvQ');
 if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-1002232703186');
-if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002315013894');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002474159521);
 
-if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'sql12731149');
-if (!defined('DB_HOST'))                        define('DB_HOST', 'sql12.freesqldatabase.com');
-if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'sql12731149');
-if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'HT6CXZwi5c');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'sql5762304);
+if (!defined('DB_HOST'))                        define('DB_HOST', 'sql5.freesqldatabase.com');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'sql5762304);
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'ZU6GYaPTTj);
