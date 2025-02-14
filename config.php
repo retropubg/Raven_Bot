73b7e3078a760d2fef7128a4f5de80986e198381
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-14 16:39:41 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,9 +4,9 @@
 if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'Retro');
 if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'eretro_7');
 if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6699273462');
-if (!defined('BOT_NAME')) define('BOT_NAME', 'retrobot');
-if (!defined('BOT_USERNAME')) define('BOT_USERNAME', 'retrochkbot');
-if (!defined('BOT_TOKEN')) define('BOT_TOKEN', '7633669044:AAHiS9PWSZkyZeHdTA6m8-6PYNf9DKitfvQ');
+if (!defined('BOT_NAME')) define('BOT_NAME', 'Totot');
+if (!defined('BOT_USERNAME')) define('BOT_USERNAME', 'qjjwjqjajsisudbot);
+if (!defined('BOT_TOKEN')) define('BOT_TOKEN', '8048311747:AAGyGx8dCxU3zsDsct5Hd6T6Ign5G6gVq6Y');
 if (!defined('BOT_LOGS')) define('BOT_LOGS', '-1002232703186');
 if (!defined('BOT_GROUP')) define('BOT_GROUP', '-1002315013894');
 
