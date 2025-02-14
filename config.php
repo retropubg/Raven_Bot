54b582212d9e47e59b3577d174af5cdac18f9c72
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-14 16:45:39 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,13 @@
-
 <?php
 
-if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'Retro');
-if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'eretro_7');
-if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6699273462');
+if (!defined('BOT_OWNER_NAME')) define('BOT_OWNER_NAME', 'Retro');
+if (!defined('BOT_OWNER_USERNAME')) define('BOT_OWNER_USERNAME', 'eretro_7');
+if (!defined('BOT_OWNER_ID')) define('BOT_OWNER_ID', '6699273462');
 if (!defined('BOT_NAME')) define('BOT_NAME', 'Totot');
-if (!defined('BOT_USERNAME')) define('BOT_USERNAME', 'qjjwjqjajsisudbot);
+if (!defined('BOT_USERNAME')) define('BOT_USERNAME', 'qjjwjqjajsisudbot');
 if (!defined('BOT_TOKEN')) define('BOT_TOKEN', '8048311747:AAGyGx8dCxU3zsDsct5Hd6T6Ign5G6gVq6Y');
 if (!defined('BOT_LOGS')) define('BOT_LOGS', '-1002232703186');
 if (!defined('BOT_GROUP')) define('BOT_GROUP', '-1002315013894');
-
 
 if (!defined('DB_DATABASE')) define('DB_DATABASE', 'sql5762304');
 if (!defined('DB_HOST')) define('DB_HOST', 'sql5.freesqldatabase.com');
