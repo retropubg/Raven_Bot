c7e4d899c84e49b8c79786fb5f69240cc7706075
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-14 22:40:53 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
-if (!defined('BOT_OWNER_NAME')) define('BOT_OWNER_NAME', getenv('BOT_OWNER_NAME'));
-if (!defined('BOT_OWNER_USERNAME')) define('BOT_OWNER_USERNAME', getenv('BOT_OWNER_USERNAME'));
-if (!defined('BOT_OWNER_ID')) define('BOT_OWNER_ID', getenv('BOT_OWNER_ID'));
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'RetroBot');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'eretro_7');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6699273462');
 
-if (!defined('BOT_NAME')) define('BOT_NAME', getenv('BOT_NAME'));
-if (!defined('BOT_USERNAME')) define('BOT_USERNAME', getenv('BOT_USERNAME'));
-if (!defined('BOT_TOKEN')) define('BOT_TOKEN', getenv('BOT_TOKEN'));
-if (!defined('BOT_LOGS')) define('BOT_LOGS', getenv('BOT_LOGS'));
-if (!defined('BOT_GROUP')) define('BOT_GROUP', getenv('BOT_GROUP'));
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'retrobot');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'retrochkbot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7633669044:AAHiS9PWSZkyZeHdTA6m8-6PYNf9DKitfvQ');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-1002333022875);
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002474159521');
 
-if (!defined('DB_DATABASE')) define('DB_DATABASE', getenv('DB_DATABASE'));
-if (!defined('DB_HOST')) define('DB_HOST', getenv('DB_HOST'));
-if (!defined('DB_USERNAME')) define('DB_USERNAME', getenv('DB_USERNAME'));
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', getenv('DB_PASSWORD'));
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'sql12731149');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'sql12.freesqldatabase.com');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'sql12731149');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'HT6CXZwi5c');
