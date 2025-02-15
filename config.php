9ead55818d8d9b72a17376d0780f964c8d2c41d7
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-14 22:22:05 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
-<?php
+if (!defined('BOT_OWNER_NAME')) define('BOT_OWNER_NAME', getenv('BOT_OWNER_NAME'));
+if (!defined('BOT_OWNER_USERNAME')) define('BOT_OWNER_USERNAME', getenv('BOT_OWNER_USERNAME'));
+if (!defined('BOT_OWNER_ID')) define('BOT_OWNER_ID', getenv('BOT_OWNER_ID'));
 
+if (!defined('BOT_NAME')) define('BOT_NAME', getenv('BOT_NAME'));
+if (!defined('BOT_USERNAME')) define('BOT_USERNAME', getenv('BOT_USERNAME'));
 if (!defined('BOT_TOKEN')) define('BOT_TOKEN', getenv('BOT_TOKEN'));
+if (!defined('BOT_LOGS')) define('BOT_LOGS', getenv('BOT_LOGS'));
+if (!defined('BOT_GROUP')) define('BOT_GROUP', getenv('BOT_GROUP'));
+
+if (!defined('DB_DATABASE')) define('DB_DATABASE', getenv('DB_DATABASE'));
 if (!defined('DB_HOST')) define('DB_HOST', getenv('DB_HOST'));
 if (!defined('DB_USERNAME')) define('DB_USERNAME', getenv('DB_USERNAME'));
 if (!defined('DB_PASSWORD')) define('DB_PASSWORD', getenv('DB_PASSWORD'));
-if (!defined('DB_DATABASE')) define('DB_DATABASE', getenv('DB_DATABASE'));
