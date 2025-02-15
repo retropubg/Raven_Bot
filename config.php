fb405dbd49406d81439255a8c3d0e10c2fa456a7
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-14 22:42:41 -0400

    Update config.php

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7633669044
 if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-1002333022875');
 if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002474159521');
 
-if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'sql12731149');
-if (!defined('DB_HOST'))                        define('DB_HOST', 'sql12.freesqldatabase.com');
-if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'sql12731149');
-if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'HT6CXZwi5c');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'sql5762304');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'sql5.freesqldatabase.com');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'sql5762304');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'ZU6GYaPTTj');
