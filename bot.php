b2fbbd0191e9e98693c50863da6d74f845ce3fb9
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-15 19:01:04 -0400

    Update bot.php

--- bot.php
+++ bot.php
@@ -78,9 +78,7 @@ $person = empty($usern_n) ? "<a href='tg://openmessage?user_id={$user_id}'>{$use
 if (!$cmd = $bot->cmd($message)) exit;
 
 if (substr($cmd->data, 0, 1) == '@' && strpos(strtolower($cmd->data), '@'.strtolower(BOT_USERNAME)) === false) exit;
-
-
-	exit;
+ exit;
 }
 
 
